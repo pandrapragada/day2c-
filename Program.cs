f6592d4a34c44abdfad9cf1d38241bda5e58e387
003D2V744 <003D2V744@IBM-PF3BT2Y6>
2022-03-01 12:57:28 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace myfirstconsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //class method
+           
+            // swap two number
+            int a = 10, b = 20;
+            Console.WriteLine("value of a :" + a);
+            Console.WriteLine(" value of b:" + b);
+            a = a * b;
+            b = a / b;
+            a = a / b;
+            Console.WriteLine("after swapping");
+            Console.WriteLine("value of a:" + a);
+            Console.WriteLine("employee details");
+            String name = "himabindhu";
+            Console.WriteLine("value of b:" + b);
+            Console.ReadLine();//
+            //program about employee details
+            String id = "003d2v";
+            Console.WriteLine("employee name :" + name);
+            Console.WriteLine(" employee id:" + id);
+            Console.ReadLine();
+
+
+        }
+    }
+}
+
+
+
+  
+
+
+
+
