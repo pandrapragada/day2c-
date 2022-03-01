b565926663884afd3108d17f269100e01c05a465
Pandrapragada Himabindhu <Pandrapragada.Himabindhu@ibm.com>
2022-03-01 15:52:30 +0530

    commit

--- Program.cs
+++ Program.cs
@@ -7,25 +7,92 @@ namespace myfirstconsole
         static void Main(string[] args)
         {
             //class method
-           
-            // swap two number
-            int a = 10, b = 20;
-            Console.WriteLine("value of a :" + a);
-            Console.WriteLine(" value of b:" + b);
-            a = a * b;
-            b = a / b;
-            a = a / b;
-            Console.WriteLine("after swapping");
-            Console.WriteLine("value of a:" + a);
-            Console.WriteLine("employee details");
-            String name = "himabindhu";
-            Console.WriteLine("value of b:" + b);
-            Console.ReadLine();//
-            //program about employee details
-            String id = "003d2v";
-            Console.WriteLine("employee name :" + name);
-            Console.WriteLine(" employee id:" + id);
-            Console.ReadLine();
+    
+                int num1, num2,num3;
+                num1 = 10;
+                num2 = 20;
+                num3 = 50;
+                if (num1 > num2)
+                {
+                    if (num1 > num3)
+                    {
+                        Console.Write("Number one is the largest!\n");
+                    }
+                    else
+                    {
+                        Console.Write("Number three is the largest!\n");
+                    }
+                }
+                else if (num2 > num3)
+                    Console.Write("Number two is the largest!\n");
+                else
+                    Console.Write("Number three is the largest!\n");
+                Console.ReadLine();
+
+                // swap two number
+                //int a = 10, b = 20;
+                //Console.WriteLine("value of a :" + a);
+                //Console.WriteLine(" value of b:" + b);
+                //a = a * b;
+                //b = a / b;
+                //a = a / b;
+                //Console.WriteLine("after swapping");
+                //Console.WriteLine("value of a:" + a);
+                //Console.ReadLine();
+                //write emoloyee details
+                //Console.WriteLine("employee details");
+                //String name = "himabindhu";
+                //String id = "003d2v";
+                //Console.WriteLine("employee name :" + name);
+                //Console.WriteLine(" employee id:" + id);
+                //Console.ReadLine();
+                //switch case calculator
+                //double num1, num2, result = 0;
+
+                //Console.Write("Enter first number:");
+                //num1 = Convert.ToDouble(Console.ReadLine());
+
+                //Console.Write("Enter second number:");
+                //num2 = Convert.ToDouble(Console.ReadLine());
+
+                //Console.Write("Enter an operator (+, -, *, /): ");
+                //char operand = Console.ReadKey().KeyChar;
+                //Console.WriteLine();
+
+                //switch (operand)
+                //{
+                //    case '+':
+                //        result = num1 + num2;
+                //        break;
+
+                //    case '-':
+                //        result = num1 - num2;
+                //        break;
+
+                //    case '*':
+                //        result = num1 * num2;
+                //        break;
+
+                //    case '/':
+                //        result = num1 / num2;
+                //        break;
+                //    default:
+                //        Console.WriteLine("You have entered wrong operator");
+                //        break;
+                //}
+
+                //Console.WriteLine(num1 + " " + operand + " " + num2 + ": " + result);
+                //Console.ReadLine();
+                //sum of natural numbers
+            //    int n = 10;
+            //int sum = 0;
+            //for(int i=1;i<=n;i++)
+            //    sum += i;
+            //Console.WriteLine("sum is:" + sum);
+            //Console.ReadLine();
+       
+
+
 
 
         }
